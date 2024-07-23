3f15172a4c468d5492b5b3f4ef707155f50d5aa8
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:37:40 -0500

    EjerciciosPromedio

---
+++ PromedioTresNumeros.py
@@ -0,0 +1,11 @@
+# Escribe un programa que solicite al usuario ingresar tres numeros
+# para luego mostrale el promedio de los tres. 
+
+N1 = int(input("Ingrese el primer mumero: "))
+N2 = int(input("Ingrese el segundo mumero: "))
+N3 = int(input("Ingrese el tercer mumero: "))
+R = N1 + N2 + N3
+P = R / 3
+
+print("El resultado de la suma es:" , R)
+print("El promedio es:" , P)
