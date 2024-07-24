5012eec91ba7fcdec873c0a7276c4c0efad48bde
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:00:53 -0500

    nuevo ejercicios

---
+++ Porcentaje.py
@@ -0,0 +1,10 @@
+# Escribe un pograma que solicite al usuario
+# un numero y le reste el 15%, almacenado
+# todo en una unica variable. A continuacion
+# mostrar el resultado final en pantalla
+
+N = int(input("Ingese un numero: "))
+Por = N * 15
+Div = Por / 100
+Res = N - Div
+print(Res)
