611e5ae257e7a223d3bc9d2b50cbe9211f3fade5
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:38:20 -0500

    nuevo

---
+++ Concatenar.py
@@ -0,0 +1,8 @@
+# Escribe un pograma que solicite al usuario el ingreso de dos palabras
+# las cuales se guardaran en dos variable distintas.
+# A coninuacion, almacena en una variable la contatenacion de la primera palabra,
+# mas un espaci, mas la segunda palabra. Muestre el resultado en pantalla.
+
+pal1 = input("Ingresar la primera palabra: ")
+pal2 = input("Ingrese la segunda palabra: ")
+print(pal1 + " , " + pal2)
