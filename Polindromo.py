b922f80957d3104a3b13cc0c165c600625e10216
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-29 21:37:54 -0500

    nuevos

---
+++ Polindromo.py
@@ -0,0 +1,5 @@
+poli = input("Ingrese una palabra: ").lower()
+if poli == poli[::-1]:
+  print("Es un Palíndromo")
+else:
+  print("No es un Palíndromo")
---
+++ Valor.py
@@ -0,0 +1,13 @@
+
+numeros = [1]
+enteros = numeros[:]
+enteros.append(2)
+print(numeros)
+print(enteros)
+
+# copia individuales
+numerosIndividuales = [1]
+enterosIndividuales = numerosIndividuales[:]
+enterosIndividuales.append(2)
+print(numerosIndividuales)
+print(enterosIndividuales)
